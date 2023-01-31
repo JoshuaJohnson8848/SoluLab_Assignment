a94bc05579a7710bafe98614ce7f558b2585abce
Joshua Johnson <joshuatjohnson255@gmail.com>
2023-01-31 23:03:30 +0530

    Completed Create Product Feature

--- controller/product.js
+++ controller/product.js
@@ -7,7 +7,7 @@ exports.createProduct = async (req, res, next) => {
     const unitPrice = req.body.unitPrice;
     const unitInStock = req.body.unitInStock;
     const discontinued = req.body.discontinued;
-    const categoryId = '7898jb989u09n';
+    const categoryId = req.body.categoryId;
 
     const product = await new Product({
       productName: productName,
@@ -19,6 +19,9 @@ exports.createProduct = async (req, res, next) => {
     });
 
     const createdProduct = await product.save();
+    res
+      .status(200)
+      .json({ message: 'Product Created', product: createdProduct });
   } catch (err) {
     console.log(err);
   }
--- router/product.js
+++ router/product.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const productController = require('../controller/product');
 
-router.post('');
+router.post('', productController.createProduct);
 
 module.exports = router;
